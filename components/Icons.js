102259ecf33034d39c91f5a483c285d5caa8a438
Ben Smith <benedictsmith@gmail.com>
2017-11-01 22:16:34 +0100

    Tweak icons

--- components/Icons.js
+++ components/Icons.js
@@ -1,25 +1,28 @@
 import React from "react";
 import styled from "react-emotion";
+import { space } from 'styled-system'
+import { withProps } from 'recompose'
 
-const Svg = styled("svg")`
+const Svg = withProps({ width: 24, height: 24 })(styled("svg")`
   stroke: transparent;
   fill: white;
-`;
+  ${space}
+`)
 
-export const Twitter = () => (
-  <Svg width={24} height={24}>
+export const Twitter = (props) => (
+  <Svg {...props}>
     <path d="M24 4.557a9.83 9.83 0 0 1-2.828.775 4.932 4.932 0 0 0 2.165-2.724 9.864 9.864 0 0 1-3.127 1.195 4.916 4.916 0 0 0-3.594-1.555c-3.179 0-5.515 2.966-4.797 6.045A13.978 13.978 0 0 1 1.671 3.149a4.93 4.93 0 0 0 1.523 6.574 4.903 4.903 0 0 1-2.229-.616c-.054 2.281 1.581 4.415 3.949 4.89a4.935 4.935 0 0 1-2.224.084 4.928 4.928 0 0 0 4.6 3.419A9.9 9.9 0 0 1 0 19.54a13.94 13.94 0 0 0 7.548 2.212c9.142 0 14.307-7.721 13.995-14.646A10.025 10.025 0 0 0 24 4.557z" />
   </Svg>
 );
 
-export const Facebook = () => (
-  <Svg width={24} height={24}>
+export const Facebook = (props) => (
+  <Svg {...props}>
     <path d="M22.675 0H1.325C.593 0 0 .593 0 1.325v21.351C0 23.407.593 24 1.325 24H12.82v-9.294H9.692v-3.622h3.128V8.413c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.795.715-1.795 1.763v2.313h3.587l-.467 3.622h-3.12V24h6.116c.73 0 1.323-.593 1.323-1.325V1.325C24 .593 23.407 0 22.675 0z" />
   </Svg>
 );
 
-export const Menu = () => (
-  <Svg width={24} height={24}>
+export const Menu = (props) => (
+  <Svg {...props}>
     <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10S2 17.514 2 12 6.486 2 12 2zm0-2C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zm6 17H6v-2h12v2zm0-4H6v-2h12v2zm0-4H6V7h12v2z" />
   </Svg>
 );
